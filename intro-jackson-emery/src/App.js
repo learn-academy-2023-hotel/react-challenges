84441a7e5152240755e1b7101fe14abbd0956bed
EWinkeljohn <emcwink@gmail.com>
2023-10-23 11:50:54 -0700

    second commit

--- intro-jackson-emery/src/App.js
+++ intro-jackson-emery/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header.js'
+import Profile from './components/Profile.js'
 
 const App = () => {
   return (
     <>
     <Header />
+    <Profile />
     </>
   )
 }
