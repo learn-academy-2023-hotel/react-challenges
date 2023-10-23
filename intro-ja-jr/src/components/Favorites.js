de44241edaf0bfe39427bd6a5ae862a95cafa4e2
AragonJ13 <jeanaragon0@gmail.com>
2023-10-23 11:41:40 -0700

    app 1st commit

---
+++ intro-ja-jr/src/components/Favorites.js
@@ -0,0 +1,17 @@
+
+
+const Favorites = () => {
+    return (
+    <> 
+     <ul>
+        <li> Break Bad   </li>
+        <li> Better Call Saul   </li>
+        <li>  Sons Of Anarchy  </li>
+        <li>  Game Of Thrones </li>
+        <li>  Wednesday  </li>
+    </ul>
+    </>
+    )
+}
+
+export default Favorites
