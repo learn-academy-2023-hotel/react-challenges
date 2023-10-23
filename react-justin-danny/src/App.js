3dda2f2c7107b9d8101de94b69594b85dcff1528
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-10-23 10:31:08 -0700

    React Cahhlenges

---
+++ react-justin-danny/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Header from "./components/Header.js"
+import Profile from "./components/Profile.js"
+import Favorite from "./components/Favorite.js"
+import Footer from "./components/Footer.js"
+
+const App = () => {
+  return(
+    <>
+      <Header />
+      <Profile />
+      <Favorite />
+      <Footer />
+    </>
+  )
+}
+
+export default App 
