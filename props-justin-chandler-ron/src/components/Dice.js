c3c133290951bb818af88c4cf2c05a5861bb288f
rrmorant <duecel@icloud.com>
2023-10-25 15:07:20 -0700

    Initial Code

---
+++ props-justin-chandler-ron/src/components/Dice.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Dice = () => {
+    return(
+        <div className="box">
+            <div className="insideBox">
+                <div className="dice">
+            2
+                </div>
+            </div>
+            <div className="insideBox">
+            
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Dice
