5613f37e9f8620da57d19a4e490408c6b083810e
JacksonPruett <jacksonc.pruett@gmail.com>
2023-10-26 16:33:10 -0700

    almost done

--- functional-props-jackson-alex/src/components/FoodData.js
+++ functional-props-jackson-alex/src/components/FoodData.js
@@ -4,9 +4,11 @@ const FoodData = (props) => {
     return (
         <>
         <p>{props.food.foodItem} {props.food.price}</p>
+        {props.food.selected && <p>selected!</p>}
         <button onClick={() => props.foodSelect(props.index)}>Select</button>
         </>
     )
 }
+
 
 export default FoodData
