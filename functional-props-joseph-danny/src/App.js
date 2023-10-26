1877fb34f81ef7975b37a23273c4ee0d8ad29319
Danny De La Rosa <danny.devlarosa@gmail.com>
2023-10-26 16:41:32 -0700

    functional challenege

--- functional-props-joseph-danny/src/App.js
+++ functional-props-joseph-danny/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import FoodData from './components/FoodData'
+import Cart from './components/Cart'
 
 
 const App = () => {
@@ -7,23 +8,33 @@ const App = () => {
   const [foodList, setfoodList] = useState([
     { title: "Burger", price: 5.00, click: false},
     { title: "Pizza", price: 4.00, click: false},
-    { title: "French Fries", price: 2.00, click: false}
-    { title: "Taco", price: 1.00, click: false}
+    { title: "French Fries", price: 2.00, click: false},
+    { title: "Taco", price: 1.00, click: false},
     { title: "Burrito", price: 4.50, click: false}
   ])
+
+  // const [cart, setCart] = useState([])
+
+  const clickedFood = (selectedFood) => {
+    foodList[selectedFood].click=true
+    // setCart([...cart, foodList[selectedFood]])
+    // setfoodList([...foodList])
+  }
+
   return (
     <>
     <h1> Food Menu</h1>
     {foodList.map((food, index) => {
       return (
         <FoodData
-        showObj={food}
+        foodObj={food}
         key={index}
-        likeShow={likeFood}
+        clickedFood={clickedFood}
         index={index}
         />
       )
     })}
+    <Cart />
     </>
     )
     // I can click a button to select an item I want to order
--- functional-props-joseph-danny/src/components/FoodData.js
+++ functional-props-joseph-danny/src/components/FoodData.js
@@ -1,11 +1,16 @@
 import React from 'react'
+import Cart from './Cart'
 
 const FoodData = (props) => {
-
+const handleClick = () => {
+    props.clickedFood(props.index)
+}
 return (
 <>
-{/* <h3>{props.showObj.title}</h3> */}
-{/* <button onClick={handleClick}>Food</button> */}
+<h3>{props.foodObj.title}</h3>
+<h3>{props.foodObj.price}</h3>
+<h4>{props.foodObj.click}</h4>
+<button onClick={handleClick}>Order</button>
 </>
 )
 }
