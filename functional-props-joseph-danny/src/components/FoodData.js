a8ba78a8cd085f2af31a0a78f987d0afd8198985
JosephMcMorrisII <josephmcmorris14@gmail.com>
2023-10-26 14:29:48 -0700

    initial push

---
+++ functional-props-joseph-danny/src/components/FoodData.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FoodData = (props) => {
+
+return (
+<>
+{/* <h3>{props.showObj.title}</h3> */}
+{/* <button onClick={handleClick}>Food</button> */}
+</>
+)
+}
+
+export default FoodData
