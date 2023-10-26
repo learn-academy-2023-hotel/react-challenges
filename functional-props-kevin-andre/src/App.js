6fb72f775db5b6c0646b3ad3d58e26b0ef27a22c
LEARN Academy <learnacademy@MacBook-Air.local>
2023-10-26 15:54:24 -0700

    second commit with button that does nothing

--- functional-props-kevin-andre/src/App.js
+++ functional-props-kevin-andre/src/App.js
@@ -1,22 +1,35 @@
 import React, { useState } from 'react'
-import HotFood from "./components/HotFood"
+import HotFood from './components/HotFood'
+
+
 
 const App = () => {
 
   // let food = [{ item: 'hotdog', price: .99 }, { item: 'hamburger', price: 1.00 }, { item: 'fries', price: .50 }]
 
-  const [foodItems, setfoodItems] = useState([{ item: 'hotdog', price: .99 }, { item: 'hamburger', price: 1.00 }, { item: 'fries', price: .50 }])
+  const [foodItems, setFoodItems] = useState([
+    { item: 'hotdog', price: .99 }, 
+    { item: 'hamburger', price: 1.00 }, 
+    { item: 'fries', price: .50 },
+    { item:'pizza', price: 2.99},
+    { item:'spaghetto', price: 0.10},
+    { item:'spaghetti', price: 1.99},
+    { item:'nachos', price: 3.99},
+    { item:'chips', price: .99},
+    { item:'cake', price: 2.99},
+    { item:'drinks', price: .99},
 
-
-
+  ])
+ 
   return (
     <>
       <h1> Challenge: Food Ordering App </h1>
       {foodItems.map((value, index) => {
-        return <HotFood showObj={value} key={index} />
+        return <HotFood showObj={value} key={index} index = {index} />
       })}
-    </>  
+    </>
   )
+
 }
 
 export default App
--- functional-props-kevin-andre/src/components/HotFood.js
+++ functional-props-kevin-andre/src/components/HotFood.js
@@ -2,13 +2,19 @@ import React from 'react'
 
 const HotFood = (props) =>{
     console.log(props)
+    const selectedFood = () => {
+        return
+       
+       
+    }
 
     return(
     <>
-        {props.showObj.item} 
-        {props.showObj.price}
+     
+      {props.showObj.item},{props.showObj.price}
+        <button onClick={selectedFood}>Select</button>
        
-    </>
+     </>
     )
 }
 export default HotFood
