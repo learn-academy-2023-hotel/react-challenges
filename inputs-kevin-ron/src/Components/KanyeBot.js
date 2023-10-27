46d8d475b061134d9736e6cd7de3acd86edd7fbf
kevinrojas <k3vinr07@gmail.com>
2023-10-27 11:52:36 -0700

    almost done

---
+++ inputs-kevin-ron/src/Components/KanyeBot.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const KanyeRobot = (props) =>{
+    return (
+        <>
+        <div className = "robot-box">
+        <h3> Kanye Robot</h3>
+        I'm gonna let you finish but Beyonce is {props.userInput}
+
+        </div>
+
+
+        </>
+
+    )
+}
+
+
+export default KanyeRobot
