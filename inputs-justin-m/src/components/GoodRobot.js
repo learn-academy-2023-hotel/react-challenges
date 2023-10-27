e9f6c1a56811741fe23fa1ae50f1f3f4a90aa468
jdpinkard <justin.d.pinkard@gmail.com>
2023-10-27 11:52:54 -0700

    1st commit

---
+++ inputs-justin-m/src/components/GoodRobot.js
@@ -0,0 +1,11 @@
+const GoodRobot = (props) => {
+
+    return(
+        <>
+            <h3>Good Robot</h3>
+            I hear you saying {props.name}. Is that correct?
+        </>
+    )
+}
+
+export default GoodRobot
